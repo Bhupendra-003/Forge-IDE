1e88df00604c1132689a1c8a2b6dc4ec17b640f3
Bhupendra <bhupendra.raj003@gmail.com>
2025-03-31 21:30:03 +0530

    added clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-      <div>
-        <h1>Home</h1>
-      </div>
+    <div>
+      <SignedOut>
+        <button className="bg-blue-500 text-white font-semibold m-8 px-4 py-2 rounded">
+          <SignInButton />
+        </button>
+      </SignedOut>
+      <SignedIn>
+        <button className="bg-red-500 text-white font-semibold m-8 px-4 py-2 rounded">
+          <SignOutButton />
+        </button>
+      </SignedIn>
+    </div>
   )
 }
