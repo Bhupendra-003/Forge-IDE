f80e5d2848698e2b8bf7e51c2769e213fff9e9b0
Bhupendra <bhupendra.raj003.github@gmail.com>
2025-07-17 20:05:32 +0530

    mouseWheelZoom

--- src/app/(root)/_components/EditorPanel.tsx
+++ src/app/(root)/_components/EditorPanel.tsx
@@ -74,6 +74,7 @@ function EditorPanel() {
                                 lineHeight,
                                 letterSpacing,
                                 roundedSelection,
+                                mouseWheelZoom: true,
                             }}
                         />
                     )}
